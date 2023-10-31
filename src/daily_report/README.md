e5fe88f3a3600249a310ac0ca4ab79c2576b4de7
seanbearden <72461227+seanbearden@users.noreply.github.com>
2023-10-31 16:46:14 -0700

    Access TDA with AWS secrets manager

---
+++ src/daily_report/README.md
@@ -0,0 +1,7 @@
+# Generate a Daily Report
+Using AWS SAM with Lambda
+
+## Outline
+1. Store TDA_API_KEY and TDA_ACCOUNT_ID in parameter store.
+2. Store td ameritrade token.json in AWS Secrets Manager as AMERITRADE_TOKEN_JSON. (Use client_from_login_flow or 
+   easy_client to obtain.)
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,6 @@
-from alpha_vantage_helper import get_daily_adjusted_processed
+from .alpha_vantage_helper import get_daily_adjusted_processed
+from .ameritrade_helper import get_specified_account, get_specified_account_with_aws
+from .aws_helper import get_secret
 from .pattern_helper import calculate_ichimoku, identify_multi_tops, identify_multi_bottoms
 from .os_helper import delete_files
 from .requests_helper import json_from_response
