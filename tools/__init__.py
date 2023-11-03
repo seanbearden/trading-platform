3aa7053598ec65e5b18d12a9f591c8e50b3c5114
seanbearden <72461227+seanbearden@users.noreply.github.com>
2023-11-03 13:47:19 -0700

    Table and plot in report with title page, header, and footer.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,7 @@
-from .alpha_vantage_helper import get_daily_adjusted_processed
-from .ameritrade_helper import get_specified_account, get_specified_account_with_aws
+from .alpha_vantage_helper import get_daily_adjusted_processed, find_last_crossover
+from .ameritrade_helper import get_specified_account, get_specified_account_with_aws, analyze_tda
 from .aws_helper import get_secret
+from .finviz_helper import get_screener
 from .pattern_helper import calculate_ichimoku, identify_multi_tops, identify_multi_bottoms
 from .os_helper import delete_files
 from .requests_helper import json_from_response
