2d21b1483f05c2f871256ff4ba152557db7906bd
seanbearden <72461227+seanbearden@users.noreply.github.com>
2023-11-07 18:16:32 -0800

    Added daily synopsis to PDF report.

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,6 +2,7 @@ from .alpha_vantage_helper import get_daily_adjusted_processed, find_last_crosso
 from .ameritrade_helper import get_specified_account, get_specified_account_with_aws, analyze_tda
 from .aws_helper import get_secret
 from .finviz_helper import get_screener
+from .langchain_helper import daily_synopsis
 from .pattern_helper import calculate_ichimoku, identify_multi_tops, identify_multi_bottoms
 from .os_helper import delete_files
 from .requests_helper import json_from_response
