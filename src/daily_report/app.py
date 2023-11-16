c3c71738f1cbfb5d013c580e6a59b6da33a4a3a7
seanbearden <72461227+seanbearden@users.noreply.github.com>
2023-11-16 10:55:25 -0800

    trading-platform-27 trading-platform fixes #27 Add Step Function to SAM template

--- src/daily_report/app.py
+++ src/daily_report/app.py
@@ -44,28 +44,39 @@ from tools.os_helper import delete_files
 
 
 def lambda_handler(event, context):
-    lambda_client = boto3.client('lambda')
-    synopsis_attempts = 3
-    daily_synopsis_function_arn = os.environ['DAILY_SYNOPSIS_FUNCTION_NAME']
+    # Check if the event is from DailySynopsisFunction
+    if 'statusCode' in event and 'body' in event:
+        # Deserialize the 'body' to get the actual data
+        synopsis_data = json.loads(event['body'])
 
-    for attempt in range(synopsis_attempts):
-        # Invoke DailySynopsisFunction
-        lambda_response = lambda_client.invoke(
-            FunctionName=daily_synopsis_function_arn,
-            InvocationType='RequestResponse',
-            Payload=json.dumps(event)
-        )
+        # Extract the results or handle the data as needed
+        gpt_daily_synopsis = synopsis_data.get('results', {})
 
-        lambda_payload = json.loads(lambda_response['Payload'].read())
-        lambda_payload_body = json.loads(lambda_payload.get('body', '{}'))
-        if lambda_payload.get('statusCode') == 200:
-            gpt_daily_synopsis = lambda_payload_body['results']
-            break
-        elif lambda_payload.get('statusCode') == 500:
-            error = lambda_payload_body['error']
-            gpt_daily_synopsis = f'There was an error generating the report: {error}'
-        else:
-            gpt_daily_synopsis = 'There is an unknown error occuring in DailySynopsisFunction'
+    else:
+        return {'statusCode': 500, 'body': 'Missing event dictionary!'}
+
+    # lambda_client = boto3.client('lambda')
+    # synopsis_attempts = 3
+    # daily_synopsis_function_arn = os.environ['DAILY_SYNOPSIS_FUNCTION_NAME']
+    #
+    # for attempt in range(synopsis_attempts):
+    #     # Invoke DailySynopsisFunction
+    #     lambda_response = lambda_client.invoke(
+    #         FunctionName=daily_synopsis_function_arn,
+    #         InvocationType='RequestResponse',
+    #         Payload=json.dumps(event)
+    #     )
+    #
+    #     lambda_payload = json.loads(lambda_response['Payload'].read())
+    #     lambda_payload_body = json.loads(lambda_payload.get('body', '{}'))
+    #     if lambda_payload.get('statusCode') == 200:
+    #         gpt_daily_synopsis = lambda_payload_body['results']
+    #         break
+    #     elif lambda_payload.get('statusCode') == 500:
+    #         error = lambda_payload_body['error']
+    #         gpt_daily_synopsis = f'There was an error generating the report: {error}'
+    #     else:
+    #         gpt_daily_synopsis = 'There is an unknown error occuring in DailySynopsisFunction'
 
     # gpt_daily_synopsis = daily_synopsis(temperature=1, model="gpt-4-1106-preview", verbose=True)
     # files to be removed from /tmp at end of execution
