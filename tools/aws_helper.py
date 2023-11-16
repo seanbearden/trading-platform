14b492b4de2014dc7724a2cc08b056e0afacbdda
seanbearden <72461227+seanbearden@users.noreply.github.com>
2023-11-16 15:52:57 -0800

    trading-platform-32 trading-platform fixes #32 Text every day with portfolio value
    Created positions table and lambda to put values into it

--- tools/aws_helper.py
+++ tools/aws_helper.py
@@ -2,6 +2,7 @@
 # If you need more information about configurations
 # or implementing the sample code, visit the AWS docs:
 # https://aws.amazon.com/developer/language/python/
+from decimal import Decimal
 
 import boto3
 from botocore.exceptions import ClientError
@@ -28,3 +29,16 @@ def get_secret(secret_name, region_name="us-east-1"):
     secret = get_secret_value_response['SecretString']
 
     return secret
+
+
+def convert_floats_to_decimals(obj):
+    """
+    Recursively converts float values in a dictionary to decimals.
+    """
+    if isinstance(obj, float):
+        return Decimal(str(obj))
+    elif isinstance(obj, dict):
+        return {k: convert_floats_to_decimals(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_floats_to_decimals(v) for v in obj]
+    return obj
